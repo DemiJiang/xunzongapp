0ede466bade082b37e560c22164cc0190501687f
Demi Jiang <demi.jiang9@gmail.com>
2017-11-16 13:31:27 +1100

    first commit

---
+++ Login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react-native');
+
+var Text = React.Text;
+
+var Login = React.createClass({
+    render: function(){
+        return(
+            <Text style={{color: 'red'}}>Hello</Text>
+        );
+    }
+});
+
+module.exports = Login;
---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+//import App from './Login';
+
+AppRegistry.registerComponent('xunzongapp', () => App);
